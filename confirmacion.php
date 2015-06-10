6405e559243fc5002589014ce4f4b3c77efdf6f9
cocowidmer <cwidmer@alumnos.uai.cl>
2015-06-10 18:56:57 -0400

    crecion de todas las tablas

--- confirmacion.php
+++ confirmacion.php
@@ -1,10 +1,12 @@
 <?php
 require_once(dirname(__FILE__) . '/../../config.php'); //obligatorio
-require_once($CFG->libdir.'/gradelib.php');
+/*require_once($CFG->libdir.'/gradelib.php');
 require_once($CFG->dirroot.'/user/renderer.php');
 require_once($CFG->dirroot.'/grade/lib.php');
-require_once($CFG->dirroot.'/grade/report/grader/lib.php');
-global $PAGE, $CFG, $OUTPUT, $DB, $USER;
+require_once($CFG->dirroot.'/grade/report/grader/lib.php'); */
+global $PAGE, $CFG, $OUTPUT, $DB, $USER; 
+
+
 $url = new moodle_url('/local/geoo/confirmacion.php');
 $context = context_system::instance();
 $PAGE->set_url($url);
@@ -16,8 +18,45 @@ $PAGE->navbar->add('index');
 echo $OUTPUT->header();
 echo $OUTPUT->heading('heading');
 
-$grades=grade_regrade_final_grades('2');
-var_dump($grades);
+
+echo 'hola mudno';
+
+
+function get_activity_finalgrade($activity, $activityid, $userid) {
+	global $DB;
+
+	$sql = "SELECT
+                gg.itemid, gg.userid, gg.rawscaleid, gg.finalgrade, gi.scaleid
+            FROM
+                {grade_grades} gg
+            INNER JOIN {grade_items} gi ON gi.id = gg.itemid
+            WHERE gi.itemmodule = :activity AND gi.iteminstance = :iteminstance AND gg.userid = :userid";
+	$params['activity'] = $activity;
+	$params['iteminstance'] = $activityid;
+	$params['userid'] = $userid;
+
+	$gradeitem = $DB->get_records_sql($sql, $params);
+
+	$finalgrade = 0;
+	if (!empty($gradeitem)) {
+		$gradeitem = current($gradeitem);
+
+		// Grade without scale -- grademax 100.
+		if (empty($gradeitem->scaleid)) {
+			$finalgrade = $gradeitem->finalgrade / 10;
+		} else {
+			$finalgrade = get_finalgrade_by_scale($gradeitem->finalgrade, $gradeitem->scaleid);
+		}
+	}
+
+	return $finalgrade;
+
+}
+
+var_dump($finalgrade);
+
+
+
 
 echo $OUTPUT->footer();
 ?>
--- index.php
+++ index.php
@@ -1,35 +1,182 @@
 <?php
-require_once(dirname(__FILE__) . '/../../config.php'); //obligatorio
-require_once($CFG->dirroot.'/local/geoo/forms.php');// IMPOORTANMTE
-global $PAGE, $CFG, $OUTPUT, $DB, $USER;
+require_once(dirname(__FILE__) . '/../../config.php');
+global $PAGE, $CFG, $OUTPUT, $DB;
+
+require_login();
+// Variables enviadas por URL desde el bloque, esto depende del boton donde se hizo blick
+// courseid es course module id, es decir, el curso donde el usuario estaba
+$courseid = required_param ( 'id', PARAM_INT ); //brings the right course id
+// action es que boton apreto el usuario en el bloque, este puede ser action = {assign, quiz, resource}
+// por defecto es "empty", es decir que si no se llega desde el bloque el plugin no despliega información
+//$action = optional_param('action','empty',PARAM_TEXT);
+// Construcción de la pagina en formato moodle
 $url = new moodle_url('/local/geoo/index.php');
 $context = context_system::instance();
-$PAGE->set_url($url);
 $PAGE->set_context($context);
+$PAGE->set_url($url);
 $PAGE->set_pagelayout('standard');
-$PAGE->set_heading('Titulo');
-$PAGE->navbar->add('Titulo');
-$PAGE->navbar->add('index');
+$context = context_system::instance();
+$PAGE->set_context($context);
+$PAGE->set_url($url);
+$PAGE->set_pagelayout('standard');
+$title = "Ranking";
+$PAGE->set_title($title);
+$PAGE->set_heading($title);
+
 echo $OUTPUT->header();
-echo $OUTPUT->heading('heading');
 
-echo 'holi';
-//$grades=grade_get_course_grade('2');
-//var_dump($grades);
 
-/*$formulario = new comment();
-if ($formulario->is_cancelled()) {
-	echo 'Usted no ingreso comentarios';
-} else if ($fromform = $formulario->get_data()) {
-	$record = new stdClass();
-	$record->comentario = $fromform->comment;
-	$record->alumno_id = $USER->id;
-	$DB->insert_record('local_geoo', $record);
+$context = get_context_instance(CONTEXT_COURSE,$courseid);
+$esProfesor = false;
+if ($roles = get_user_roles($context, $USER->id)) {
+	foreach ($roles as $role) {
+		if($role->shortname == 'editingteacher'){
+			$esProfesor = true;}}}
 
-	redirect($CFG->dirroot.'/local/geoo/confirmacion.php');
 
-} else {
-	$formulario->display();
-}*/
+$ranking = required_param ( 'ranking', PARAM_INT );
+
+if ($ranking == 1) // TAREAS, SHOWS AVERAGE OF ASSIGNMENT OF THE COURSE OF EVERY STUDENT
+{
+	
+
+	if($esProfesor){ 	
+$items = $DB->get_records_sql ( "SELECT firstname, lastname, AVG(ag.grade) as average
+			FROM mdl_assign_grades as ag INNER JOIN
+			mdl_assign as a ON (a.id = ag.assignment)
+			INNER JOIN mdl_user as u ON (ag.userid = u.id)
+			WHERE a.course = $courseid
+			GROUP BY firstname, lastname
+			ORDER BY average  DESC" );
+// new table that shows position and grades by student
+
+$table = new html_table ();
+$table->head = array (
+		'Position',
+		'Average',
+		'Firstname',
+		'Lastname'
+);
+
+$position = 0;
+foreach ( $items as $item ) {
+	$position++;
+	$firstname = $item->firstname;
+	$lastname = $item->lastname;
+	$average = $item->average;
+	$table->data[] = array($position, $average, $firstname, $lastname);
+}
+
+echo html_writer::table($table);
+	}
+
+else
+{echo "hola no puedes ver esto!";}
+
+
+} 
+
+
+
+
+
+else if ($ranking == 2) // NOTAS, SHOWS AVERAGE OF THE COURSE OF EVERY STUDENT
+{
+	if($esProfesor){
+      $items = $DB->get_records_sql ("SELECT
+			firstname, lastname, finalgrade
+			FROM
+			mdl_grade_grades as gg INNER JOIN
+			(mdl_grade_items as gi JOIN mdl_course as c JOIN mdl_user as u)
+			ON (gg.itemid = gi.id AND gi.courseid= $courseid AND gg.userid = u.id)
+			GROUP BY firstname, lastname
+			ORDER BY finalgrade DESC");
+	
+	
+	// new table that shows position and grades by student
+	
+	$table = new html_table ();
+	$table->head = array (
+		'Position',
+		'Average of Course',
+		'Firstname',
+		'Lastname'
+	);
+	
+	$position = 0;
+	foreach ( $items as $item ) {
+	$position++;
+	$firstname = $item->firstname;
+	$lastname = $item->lastname;
+	$finalgrade = $item->finalgrade;
+	$table->data[] = array($position, $finalgrade, $firstname, $lastname);
+	}
+	
+	echo html_writer::table($table);
+
+
+}
+
+else
+{echo "hola no puedes ver esto!";}
+
+}
+	
+
+else if ($ranking == 3) // ACTIVIDADES, SHOWS THE TOTAL OF FORUMS WRITEN AND FILES DOWNLOADED OF EVERY STUDENT OF THE COURSE
+{
+	if($esProfesor){
+	$items = $DB->get_records_sql ("SELECT firstname, lastname, COUNT(lsl.objectid) as sumaarchivos,
+		IFNULL (T.sumaforos,0) as sumaforos, 
+		(COUNT(lsl.objectid) + IFNULL (T.sumaforos,0)) as suma
+		FROM mdl_logstore_standard_log as lsl 
+		INNER JOIN mdl_user as u ON (lsl.userid = u.id) 
+		INNER JOIN (mdl_modules as m JOIN mdl_course_modules as cm JOIN mdl_resource as r) 
+		ON (cm.module = m.id AND r.course = cm.course AND r.id=lsl.objectid) 
+		LEFT JOIN
+		(SELECT COUNT(fd.id) as sumaforos, u2.id 
+			FROM mdl_forum_discussions as fd 
+			INNER JOIN mdl_user as u2
+		ON (fd.userid = u2.id and fd.course = $courseid)
+		GROUP BY u2.id) as T ON (T.id = u.id) 
+			WHERE lsl.action='viewed'
+		AND lsl.courseid= $courseid AND lsl.objecttable='resource' GROUP BY firstname, lastname ORDER BY suma DESC");
+	
+	// new table that shows position and grades by student
+	
+	$table = new html_table ();
+	$table->head = array (
+			'Firstname',
+			'Lastname',
+			'Downloaded Files',
+			'Forums Done',
+			'Total'
+			
+	);
+	
+	
+	foreach ( $items as $item ) {
+		$firstname = $item->firstname;
+		$lastname = $item->lastname;
+		$sumaarchivos= $item->sumaarchivos;
+		$sumaforos= $item->sumaforos;
+		$suma= $item->suma;
+	
+		$table->data[] = array($firstname, $lastname, $sumaarchivos, $sumaforos, $suma);
+	}
+	
+	echo html_writer::table($table);
+	
+	
+	
+	
+}
+
+else
+{echo "hola no puedes ver esto!";}
+
+}
+
+
 echo $OUTPUT->footer();
 ?>
